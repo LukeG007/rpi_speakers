7b92228db18ecf45963b241fdedeaa2004696f2d
LukeG007 <luke@gilsinger.net>
2021-07-31 18:38:16 -0400

    Added flask server file uploading

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print('Hello world')
+from flask import Flask, redirect, request
+import requests
+
+app = Flask(__name__)
+
+@app.route('/api/upload', methods=['POST'])
+def upload():
+    url = dict(request.form)['url']
+    filename = dict(request.form)['filename']
+    r = requests.get(url)
+    f = open(filename, 'w+')
+    f.truncate(0)
+    f.close()
+    f = open(filename, 'wb')
+    f.write(r.content)
+    f.close()
+    return 'OK'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
