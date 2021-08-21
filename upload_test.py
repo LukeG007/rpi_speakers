cbbb2d04f93349ac1327152d7f86d7679731c734
LukeG007 <luke@gilsinger.net>
2021-08-21 19:34:53 -0400

    Fixed bug

--- upload_test.py
+++ upload_test.py
@@ -1,5 +1,5 @@
 import requests
 
-data = {'url': r'http://192.168.3.199:8081/01%20-%20Ace%20of%20Spades.mp3', 'filename': 'ace_of_spades.mp3'}
+data = {'url': r'http://192.168.3.150:8081/hello.wav', 'filename': 'hello.wav'}
 
 requests.post('http://192.168.3.148/api/upload', data=data)
