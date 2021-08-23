894f8ea95681605221cb643a7c93c2e50dd62797
LukeG007 <luke@gilsinger.net>
2021-08-23 17:33:07 -0400

    Center cards

--- add_playlist_test.py
+++ add_playlist_test.py
@@ -1,5 +1,5 @@
 import requests
 
-data = {'name': 'Chill', 'img_url': 'https://ih1.redbubble.net/image.1234400278.2371/bg,f8f8f8-flat,750x,075,f-pad,750x1000,f8f8f8.jpg', 'img_filename': 'image.jpg'}
+data = {'name': 'Chill', 'img_url': 'http://192.168.3.150:8081/chill_cover.jpg', 'img_filename': 'cover.jpg'}
 
 requests.post('http://192.168.3.148/api/create_playlist', data=data)
